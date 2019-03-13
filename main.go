0f739e04e8d32062525b8ce8c9e5391d15054d20
Kevin Paul Herbert <kph@platinasystems.com>
2019-03-13 07:23:32 -0700

    main: compatibility with coreboot v4.9
    The -t option was deprecated in cbfstool, remove it.
    
    Signed-off-by: Kevin Paul Herbert <kph@platinasystems.com>

--- main.go
+++ main.go
@@ -504,7 +504,7 @@ func makeAmd64CorebootRom(romfile, machine string) (err error) {
 		" -f " + machine + ".vmlinuz" +
 		" -I goes-boot.cpio.xz" +
 		` -C "console=ttyS0,115200n8 intel_iommu=off quiet"` +
-		" -n fallback/payload -t payload -c none -r COREBOOT" +
+		" -n fallback/payload -c none -r COREBOOT" +
 		" && mv " + tmprom + " " + romfile +
 		" && " + cbfstool + " " + romfile + " print"
 	if err := shellCommandRun(cmdline); err != nil {
