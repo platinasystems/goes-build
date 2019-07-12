e76607424687d0262022348c06dfbdd3f184fb59
Kevin Paul Herbert <kph@platinasystems.com>
2019-07-12 14:06:42 -0700

    u-boot: Remove legacy booting, add UBIFS booting, remove IP option
    Remove support for legacy (non-ITB) booting.
    
    Add support for booting from ITB via SF and UBIFS (for pre- and post-
    migration).
    
    Remove ip= kernel command option (ip is setup in goes-bmc now).
    
    Signed-off-by: Kevin Paul Herbert <kph@platinasystems.com>

--- u-boot-envvars.go
+++ u-boot-envvars.go
@@ -8,19 +8,15 @@ import (
 )
 
 const envvar = `baudrate=115200
-bootargs=console=ttymxc0,115200n8 ip=dhcp
-bootcmd=run readmac;run sf_read_itb bootlinux_itb;run sf_read_legacy bootlinux_legacy
+bootargs=console=ttymxc0,115200n8
+bootcmd=run readmac;run sf_read_itb bootlinux_itb;run ubi_read_itb bootlinux_itb
 bootlinux_itb=bootm ${loadaddr}
-bootlinux_legacy=bootz ${loadaddr} ${initrd_high} ${fdt_high}
 bootdelay=3
 ethact=FEC
 ethprime=FEC
 fdt_high=0x88000000
 initrd_high=0x89000000
 loadaddr=0x82000000
-load_fdt_sf=sf read ${fdt_high} 0x00080000 ${sz_fdt}
-load_initramfs_sf=sf read ${initrd_high} 0x00300000 ${sz_initrd}
-load_kernel_sf=sf read ${loadaddr} 0x00100000 ${sz_kernel}
 net_read_itb=${netbootmethod} ${loadaddr} ${serverpath}platina-mk1-bmc-itb.bin
 netboot=run readmac net_read_itb bootlinux_itb
 netbootmethod=dhcp
@@ -28,13 +24,10 @@ qspi0=mw 020e01b8 00000005; mw 20a8004 c7000000; mw 020a8000 4300ca05
 qspi1=mw 020e01b8 00000005; mw 20a8004 c7000000; mw 020a8000 c300ca05
 readmac=i2c read 55 0.2 200 80800000; setmac 80800000 24; saveenv
 sf_read_itb=sf probe 0;sf read ${loadaddr} 0x00100000 ${sz_itb}
-sf_read_legacy=sf probe 0;run load_kernel_sf load_initramfs_sf load_fdt_sf
+ubi_read_itb=ubi part ubi;ubifsmount ubi0:perm;ubifsload ${loadaddr} boot/platina-mk1-bmc-itb.bin
 stderr=serial
 stdin=serial
 stdout=serial
-sz_fdt=f000
-sz_initrd=300000
-sz_kernel=200000
 sz_itb=800000
 wd=mw 020e01a0 00000005;mw 020e01a4 00000005;mw 020e01a8 00000005;mw 020e01b8 00000005;mw 020e01bc 00000005;mw 020a8000 0300ca05;mw 020a8004 07000000
 `
